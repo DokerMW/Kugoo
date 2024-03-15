53c26cb253f6fa04ea4d179eec5d036c1d301fd7
DokerMW <abddoker@gmail.com>
2024-03-15 21:54:38 +0500

    add: full header adaptive with menu

--- _src/js/_bodyLock.js
+++ _src/js/_bodyLock.js
@@ -1,7 +1,12 @@
 const body = document.querySelector('body');
 
-document.addEventListener('click', function () {
-   return modal.classList.contains('_active')
-      ? body.classList.add('_lock')
-      : body.classList.remove('_lock');
+document.addEventListener('click', function (e) {
+   if (
+      modal.classList.contains('_active') ||
+      (menuBody.classList.contains('_active') && window.innerWidth < 991.98)
+   ) {
+      body.classList.add('_lock');
+   } else {
+      body.classList.remove('_lock');
+   }
 });
